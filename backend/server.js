f8183d7c9711ddd256f95cdf798e2bda003acbf0
Yash-89 <yashproraj@gmail.com>
2025-02-24 20:56:58 +0530

    Backend Complete

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,18 @@
-// const express = require('express')
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js'
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to use JSON data in the req.body
 
-app.get('/', async (req, res) => {
-    res.send("Hello World!");
-})
+app.use("/api/products", productRoutes);
 
-app.post('/api/products', async (req, res) => {
-    const product = req.body; // user will send this data
-
-    if (!product.name || !product.price || !product.image)
-        return res.status(400).json({ success: false, message: "Please provide all the fields" });
-    
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in create Product: ", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
-
-app.delete("/api/products/id:", async (req, res) => {
-    const {id} = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product Deleted" });
-    } catch (error) {
-        res.status(404).json({ success: false, message: "Product not found" });
-    }
-})
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:" + PORT);
 });
