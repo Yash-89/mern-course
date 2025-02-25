1890976b2b6d464f5d4889a13f7baafb34802310
Yash-89 <yashproraj@gmail.com>
2025-02-25 20:44:47 +0530

    Fixed typo

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -36,7 +36,7 @@ export const updateProduct = async (req, res) => {
         return res.status(404).json({ success: false, message: "Invalid Product ID" });
 
     try {
-        const updatedProduct = await product.findByIdAndUpdate(id, product, {new:true});
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
         res.status(200).json({ success: true, data: updatedProduct })
     } catch (error) {
         res.status(500).json({ success: false, message: "Server Error" });
