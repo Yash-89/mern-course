b3289b97b9550908d803bc93d45225d83c218292
Yash-89 <yashproraj@gmail.com>
2025-02-28 00:30:57 +0530

    Create Product functionality added in frontent

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { Container, Flex, Text, HStack, Button } from '@chakra-ui/react';
+import { LuSquarePlus, LuShoppingCart } from 'react-icons/lu';
+import React from 'react';
 
 const Navbar = () => {
+  
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row"
+        }}
+      >
+        <Text
+          fontSize={{base: "3xl", sm: "40"}}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          background="linear-gradient(to right, #00d1ff, #009aff)"
+          bgClip="text"
+        >
+          <Link to='/'>Product Store</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+          <Button fontSize={20} colorScheme="blue" variant="subtle">
+            <LuSquarePlus />
+          </Button>
+          </Link>
+
+        </HStack>
+      </Flex>
+    </Container>
   )
 }
 
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { Container, VStack, Heading, Box, Input, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react"
+import { useProductStore } from "../store/product.js";
 
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const {createProduct} = useProductStore();
+
+  const handleAddProduct = async () => {
+    const { success, message } = await createProduct(newProduct);
+    if (success) {
+      setNewProduct({
+        name: "",
+        price: "",
+        image: "",
+      });
+    }
+    console.log("Success: ", success, "Message: ", message);
+  }
+
   return (
-    <div>CreatePage</div>
+    <Container maxW="container.sm" mt={8}>
+      <Flex flex={1} justify="center" align="center">
+      <VStack w="xl" spacing={8}>
+        <Heading as="h1" size="2xl" textAlign="center" mb={8}>
+          Create New Product
+        </Heading>
+        
+        <Box w="full" p={6} rounded="lg" shadow="md">
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            />
+
+            <Input
+              placeholder="Product Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            />
+
+            <Input
+              placeholder="Product Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            />
+
+            <Button colorscheme="blue" onClick={handleAddProduct} w="full">
+              Add Product
+            </Button>
+
+          </VStack>
+        </Box>
+      </VStack>
+      </Flex>
+    </Container>
   )
 }
 
