42033b80f15a87a4a16757775c38afa49e6b1e27
Yash Raj <yashproraj@gmail.com>
2025-11-01 21:28:34 +0530

    fix: origin path

--- app/backend/server.js
+++ app/backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://productstorefrontend.z29.web.core.windows.net/",
+      "https://productstorefrontend.z29.web.core.windows.net",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
