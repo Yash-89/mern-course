4cca976ef0ce3f48a7e20c4ec195537fea435819
Yash Raj <yashproraj@gmail.com>
2025-11-01 21:40:17 +0530

    removed StrictMode

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -1,17 +1,14 @@
-import App from './App.jsx';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from "@/components/ui/provider.jsx"
-import { Toaster } from '@/components/ui/toaster.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider.jsx";
+import { Toaster } from "@/components/ui/toaster.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-        <Toaster />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+      <Toaster />
+    </Provider>
+  </BrowserRouter>
+);
