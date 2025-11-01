1a383bc9438038bdd62b1e6c85351118dedf6336
Yash Raj <yashproraj@gmail.com>
2025-11-01 20:18:21 +0530

    add cors

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -7,6 +8,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json()); // allows us to use JSON data in the req.body
 
