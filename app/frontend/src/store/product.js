6535d122daea68afda3c20b2e6c9660a00d361fa
Yash Raj <yashproraj@gmail.com>
2025-11-01 20:31:34 +0530

    Modify state management for deployment

--- app/frontend/src/store/product.js
+++ app/frontend/src/store/product.js
@@ -1,38 +1,40 @@
 import { create } from "zustand";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
   createProduct: async (newProduct) => {
     if (!newProduct.name || !newProduct.price || !newProduct.image) {
-      return {success: false, message: "Please fill all fields"};
+      return { success: false, message: "Please fill all fields" };
     }
-    const res = await fetch("/api/products", {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(newProduct),
+    const res = await fetch(`${API_URL}/api/products`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
     });
     const data = await res.json();
     set((state) => ({ products: [...state.products, data.data] }));
-    return {success: true, message: "Product created successfully"};
+    return { success: true, message: "Product created successfully" };
   },
   fetchProducts: async () => {
-    const res = await fetch("/api/products");
+    const res = await fetch(`${API_URL}/api/products`);
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (pid) => {
-    const res = await fetch(`/api/products/${pid}`, {
+    const res = await fetch(`${API_URL}/api/products/${pid}`, {
       method: "DELETE",
-    })
+    });
     const data = await res.json();
-    set(state => ({ products: state.products.filter(p => p._id !== pid) }));
+    set((state) => ({ products: state.products.filter((p) => p._id !== pid) }));
     return { success: true, message: "Product deleted successfully" };
   },
   updateProduct: async (pid, updatedProduct) => {
-    const res = await fetch(`/api/products/${pid}`, {
+    const res = await fetch(`${API_URL}/api/products/${pid}`, {
       method: "PUT",
       headers: {
         "Content-type": "application/json",
@@ -40,8 +42,9 @@ export const useProductStore = create((set) => ({
       body: JSON.stringify(updatedProduct),
     });
     const data = await res.json();
-    set(state => ({ products: state.products.map(p => p._id === pid ? data.data : p) }));
+    set((state) => ({
+      products: state.products.map((p) => (p._id === pid ? data.data : p)),
+    }));
     return { success: true, message: "Product updated successfully" };
-  }
+  },
 }));
-
--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import jsconfigPaths from "vite-jsconfig-paths"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import jsconfigPaths from "vite-jsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000'
-      }
-    }
-  }
-})
+});
