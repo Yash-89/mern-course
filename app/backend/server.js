ec07d15895dee7b82c6fdeeee03a781719c70a5d
Yash Raj <yashproraj@gmail.com>
2025-11-01 21:33:49 +0530

    fix: cors middleware

--- app/backend/server.js
+++ app/backend/server.js
@@ -15,7 +15,6 @@ app.use(
       "http://localhost:5173",
       "https://productstorefrontend.z29.web.core.windows.net",
     ],
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
