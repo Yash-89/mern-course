b7534a6330afb6dd939691b830635100de9958e7
Yash Raj <yashproraj@gmail.com>
2025-11-01 21:14:42 +0530

    add frontend deployment url in origins

--- app/backend/server.js
+++ app/backend/server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://productstorefrontend.z29.web.core.windows.net/",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
